ce6f8d6534fcf2cc68f6c5e927ad251b855c3d0f
ogabek <quvvatullayev@gmail.com>
2023-07-20 20:54:20 +0500

    fix order model

--- food/models.py
+++ food/models.py
@@ -25,5 +25,6 @@ class Order(models.Model):
     phone = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField()
     def __str__(self):
         return self.name
