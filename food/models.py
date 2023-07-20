634f61729861e8ffb629401af28aff96b1950ead
ogabek <quvvatullayev@gmail.com>
2023-07-20 20:50:21 +0500

    create product model

--- food/models.py
+++ food/models.py
@@ -11,3 +11,11 @@ class SubCategory(models.Model):
     img = models.ImageField(upload_to='subcategories')
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='products')
+    price = models.IntegerField()
+    def __str__(self):
+        return self.name
