0e1e592531e9b34983a835aaa81a9083de052273
ogabek <quvvatullayev@gmail.com>
2023-07-20 20:52:29 +0500

    create order

--- food/models.py
+++ food/models.py
@@ -19,3 +19,11 @@ class Product(models.Model):
     price = models.IntegerField()
     def __str__(self):
         return self.name
+    
+class Order(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
