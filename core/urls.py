064b9f0d11ff28d1f1a0c0dd6c5a03b9ca062b3c
ogabek <quvvatullayev@gmail.com>
2023-07-20 21:21:13 +0500

    add core urls in app urls

--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('', include('food.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
