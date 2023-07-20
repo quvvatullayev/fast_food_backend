f163be617bd4d5157c46fd69cb7f35504cd2356c
ogabek <quvvatullayev@gmail.com>
2023-07-20 21:17:41 +0500

    create add urls category

--- food/urls.py
+++ food/urls.py
@@ -1,1 +1,19 @@
 from django.urls import path
+from .views.category import (
+    CategoryCreateView,
+    CategoryDeleteView,
+    CategoryDetailView,
+    CategoryListView,
+    CategoryUpdateView,
+)
+
+urlpatterns = [
+    path('category/create/', CategoryCreateView.as_view()),
+    path('category/list/', CategoryListView.as_view()),
+    path('category/detail/<int:pk>/', CategoryDetailView.as_view()),
+    path('category/update/<int:pk>/', CategoryUpdateView.as_view()),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view()),
+]
+
+    
+    
