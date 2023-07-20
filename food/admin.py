ac839cd13db84ac5dfe5bdd0ae1c44901f742848
ogabek <quvvatullayev@gmail.com>
2023-07-20 21:20:30 +0500

    add models admin

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    Category,
+    SubCategory,
+    Product,
+    Order,
+)
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Product)
+admin.site.register(Order)
+
