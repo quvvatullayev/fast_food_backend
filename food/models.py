47e115a2a362b9597ac1b57afc5e88bbe8410386
ogabek <quvvatullayev@gmail.com>
2023-07-20 20:48:06 +0500

    create sub cateory

--- food/models.py
+++ food/models.py
@@ -4,3 +4,10 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     def __str__(self):
         return self.name
+    
+class SubCategory(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='subcategories')
+    def __str__(self):
+        return self.name
