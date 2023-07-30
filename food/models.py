d1c55950ced22d41cb387cfa58465a4f37884d1c
ogabek <quvvatullayev@gmail.com>
2023-07-30 18:42:38 +0500

    fix moder order hour

--- food/models.py
+++ food/models.py
@@ -26,6 +26,6 @@ class Order(models.Model):
     address = models.CharField(max_length=255)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     count = models.IntegerField()
-    hour = models.TimeField()
+    hour = models.DateTimeField()
     def __str__(self):
         return self.name
